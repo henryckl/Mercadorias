29c59fec9de710b3249508b792a37fad6de00ce8
Henryck <henryck.leandro@gmail.com>
2019-12-23 13:52:35 -0300

    spinner adicionado

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { logout } from "../../services/auth";
 import api from "../../services/api";
+import { Spinner } from "./style";
 
 export default function Home({ history }) {
   function Logout() {
@@ -13,15 +14,14 @@ export default function Home({ history }) {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
     if (!produto || !code) {
       setError("Preencha todos os campos");
     } else {
       try {
         setLoading(true);
-        await api.post("/email", { produto, code }).then(setLoading(false));
-        window.location.reload();
+        api.post("/email", { produto, code }).then(window.location.reload());
       } catch (err) {
         setError("Houve um problema ao enviar o formulario");
       }
@@ -30,7 +30,7 @@ export default function Home({ history }) {
 
   async function fetchProducts() {
     const result = await api.get("/products");
-    setProdutos(...produtos, result.data);
+    setProdutos(produtos => produtos.concat(result.data));
   }
 
   useEffect(() => {
@@ -43,20 +43,25 @@ export default function Home({ history }) {
       <form onSubmit={handleSubmit}>
         <label htmlFor="Produto">Produto</label>
         <select name="" id="produto" onChange={e => setProduto(e.target.value)}>
-          {produto ? null : <option value="">Selecione o produto</option>}
-          {produtos.map(p => (
-            <option value={p.name}>{p.name}</option>
+          {!produto && <option value="">Selecione o produto</option>}
+          {produtos.map((p, index) => (
+            <option key={index} value={p.name}>
+              {p.name}
+            </option>
           ))}
         </select>
         <label htmlFor="Codigo">Codigo</label>
         <select name="" id="codigo" onChange={e => setCode(e.target.value)}>
           {code ? null : <option value="">Selecione o Codigo</option>}
-          {produtos.map(p => (
-            <option value={p.code}>{p.code}</option>
+          {produtos.map((p, index) => (
+            <option key={index} value={p.code}>
+              {p.code}
+            </option>
           ))}
         </select>
         <button type="submit">Enviar</button>
         {error && <p>{error}</p>}
+        {loading && <Spinner />}
       </form>
     </>
   );
